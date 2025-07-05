8ae584841088089d76958e8d385a2810b4c9dc81
Oajas Bhagwat <oajasbhagwat5@gmail.com>
2025-07-05 12:50:50 +0530

    Update LCD_Touch.cpp

--- AutoD_DSU_NoSD_2024Dec30/LCD_Touch.cpp
+++ AutoD_DSU_NoSD_2024Dec30/LCD_Touch.cpp
@@ -310,8 +310,9 @@ void calc_Batt() {
   if (Fpr == 'H') lcd1.print("F2");
   if (Fpr == 'J') lcd1.print("F4");
   lcd1.setCursor(13, 0);
-  lcd1.print("E=");
+  lcd1.print("BE=");
   lcd1.print(BattV, 2);  // now,'Sigma',1lcd1.clear();show at (13,0) B=12.68
+  delay(2000);
   lcd1.setCursor(0, 3);
   lcd1.print("                   ");  // erase line-3 (press measure)
                                       //-----------------------show switch positions, only if RcBf_R1==3,i.e.Rannge switch pos. is on 'Bat'---------------------
